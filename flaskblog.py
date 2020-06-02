4c63d30b008f83ff1021bc53226bb48891e01981
Cesare De Cal <hi@cesare.io>
2020-06-02 10:45:44 +0200

    Created register and login forms using wtforms and added input validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, escape, request, render_template, url_for
+from flask import Flask, escape, request, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '93590f0e3dc7099d1f75f2607779ad4f'
 
 posts = [
     {
@@ -20,13 +22,28 @@ posts = [
 
 @app.route('/')
 @app.route('/home')
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 # This condition is only true if we run the script directly
